818d034e802b66639c51ca587fa3bee1fded7bb4
Akshaj Rao <117006036+cross-codes@users.noreply.github.com>
2022-11-12 15:18:10 +0530

    Added functionality for currency conversion

--- currency.py
+++ currency.py
@@ -3,8 +3,9 @@
 import argparse
 from configparser import ConfigParser
 from datetime import date
+import requests
 
-BASE_URL = "https://api.apilayer.com/exchangerates_data/convert?to={to}&from={from}&amount={amount}"
+BASE_URL = "https://api.apilayer.com/exchangerates_data/convert?to={to}&from={from_cur}&amount={amount}"
 
 
 def _get_key():
@@ -20,9 +21,24 @@ def read_user_cli_arguments():
     parser.add_argument("to", nargs="+", type=str, help="Enter the three letter currency code you want to convert to")
     parser.add_argument("amount", nargs="+", type=float, help="Enter the amount of money")
     parser.add_argument("date", nargs="?", type=date, const=cur_date, help="Enter the date (optional, for historical purposes)")
-    return parser.parse_args()
+    return parser.parse_args()  #To access the dictionary variant, use vars()
 
 
-if __name__ == "__main__":
-    read_user_cli_arguments()
+def convert(cur_from, cur_to, cur_amount, ent_date):
+    apikey = _get_key()
+    headers = { "apikey" : apikey}
+    if ent_date is None:
+        CONSTRUCTED_URL = BASE_URL.format(to = cur_to, from_cur = cur_from, amount = cur_amount)
+    response = requests.request("GET", CONSTRUCTED_URL, headers = headers)
+    if response.status_code == 200:
+        return response.json()
+
+
+
+if __name__ == "__main__":  
+    result = read_user_cli_arguments()
+    dictionary = vars(result)
+    ans_set = convert(dictionary["from"][0], dictionary["to"][0], dictionary["amount"][0], dictionary["date"])
+    print ("\n\b\b\b\b", dictionary["amount"][0], dictionary["from"][0], "is equal to", ans_set["result"], dictionary["to"][0], "as of ", "<date>")
+    print (" Current exchange rate: 1", dictionary["from"][0], "=", ans_set["info"]["rate"], dictionary["to"][0], "\n")
 
